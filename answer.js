7384a82f906e96c370c83fb87b09ce65b3f0f513
Arju-Hasan <rajuahmeed698@gmail.com>
2025-08-11 11:03:26 +0600

    update a ansewr scetion

--- answer.js
+++ answer.js
@@ -73,5 +73,24 @@ console.log("Pockat thaka jabea aj", shippingCast);
 
  //Task number 5 (Hard)
 
- 
+  const employees = [
+    { name: "shahin", experience: 5, starting: 20000, increment: 5000 },
+    { name: "shihab", experience: 3, starting: 15000, increment: 7000 },
+    { name: "shikot", experience: 9, starting: 30000, increment: 1000 },
+    { name: "shohel", experience: 0, starting: 29000, increment: 4000 },
+  ];
+  function salaryCalculator(employees) {
+    let sum = 0;
+    for (const employee of employees) {
+      let parsonTotalSalary =
+        employee.experience * employee.increment + employee.starting;
+      sum = sum + parsonTotalSalary;
+    }
+    return sum;
+  }
+
+  const totalSalary = salaryCalculator(employees);
+  console.log("Total payout par mounth", totalSalary, "taka only");
+
+  console.log(`Total payout per month: ${totalSalary} taka only`);
 
